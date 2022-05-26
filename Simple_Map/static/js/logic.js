f6b6b3857ef10781a315212312839fdde6b88613
unknown <victorpesantez_07@hotmail.com>
2022-05-25 22:01:35 -0400

    other branch

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -12,8 +12,24 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//Add a Circle to the Map
+L.circle([34.0522, -118.2437], {
+     radius : 100
+}).addTo(map);
+
+//Add a light-yellow Circle to the Map
+L.circleMarker([34.0522, -118.2437], {
+    radius : 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
