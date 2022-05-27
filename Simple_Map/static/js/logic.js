00f3d193c5ac24c941ce58c8930ac0380d84a146
unknown <victorpesantez_07@hotmail.com>
2022-05-26 21:41:10 -0400

    13.4.2

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -27,45 +27,15 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     fillColor: '#ffffa1'
 // }).addTo(map);
 
-// An array containing each city's location, state, and population.
-//Get data from cities.js
+// Get data from cities.js
 let cityData = cities;
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-  },
-  {
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-  },
-  {
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-  },
-  {
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-  },
-  {
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-  }
-  ];
 
-  // Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-    console.log(city)
-   });
+
+ // Loop through the cities array and create one marker for each city.
+// cities.forEach(function(city) {
+//   console.log(city)
+//   L.marker(city.location).addTo(map);
+// });
 
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
