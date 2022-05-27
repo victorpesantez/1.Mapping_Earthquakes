dc090c511bbbe3a60bcecd4b711f7d30c24ed5d8
unknown <victorpesantez_07@hotmail.com>
2022-05-26 22:25:33 -0400

    13.4.3

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,40 +2,20 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
 
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
-
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-//Add a Circle to the Map
-// L.circle([34.0522, -118.2437], {
-//      radius : 100
-// }).addTo(map);
-
-//Add a light-yellow Circle to the Map
-// L.circleMarker([34.0522, -118.2437], {
-//     radius : 300,
-//     color: "black",
-//     fillColor: '#ffffa1'
-// }).addTo(map);
-
-// Get data from cities.js
-let cityData = cities;
-
-
- // Loop through the cities array and create one marker for each city.
-// cities.forEach(function(city) {
-//   console.log(city)
-//   L.marker(city.location).addTo(map);
-// });
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
 
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
@@ -49,7 +29,7 @@ cityData.forEach(function(city) {
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
