9e3352fc38738db8f450db6a0ead2d4cffc6c1c1
unknown <victorpesantez_07@hotmail.com>
2022-05-26 21:26:37 -0400

    Mapping_Multiple_Points

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -16,16 +16,66 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 //Add a Circle to the Map
-L.circle([34.0522, -118.2437], {
-     radius : 100
-}).addTo(map);
+// L.circle([34.0522, -118.2437], {
+//      radius : 100
+// }).addTo(map);
 
 //Add a light-yellow Circle to the Map
-L.circleMarker([34.0522, -118.2437], {
-    radius : 300,
-    color: "black",
-    fillColor: '#ffffa1'
-}).addTo(map);
+// L.circleMarker([34.0522, -118.2437], {
+//     radius : 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+// }).addTo(map);
+
+// An array containing each city's location, state, and population.
+//Get data from cities.js
+let cityData = cities;
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
+
+  // Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+   });
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .addTo(map);
+});
 
 
 // We create the tile layer that will be the background of our map.
